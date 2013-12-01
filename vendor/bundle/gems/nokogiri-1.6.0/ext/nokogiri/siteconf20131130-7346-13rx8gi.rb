ea6c223b6abc10a0cd6b99bc3766e7904d7c21f1
Adam Henk <adam.henk@gmail.com>
2013-11-30 23:15:17 -0700

    Initial commit

---
+++ vendor/bundle/gems/nokogiri-1.6.0/ext/nokogiri/siteconf20131130-7346-13rx8gi.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131130-7346-1hm9pt2"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
