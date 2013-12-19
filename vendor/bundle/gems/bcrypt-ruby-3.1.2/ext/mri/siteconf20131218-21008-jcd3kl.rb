494cb82c1a060998563185442f215de8e6302be1
Adam Henk <adam.henk@gmail.com>
2013-12-18 22:11:39 -0700

    Make a basic User model (including secure passwords)

---
+++ vendor/bundle/gems/bcrypt-ruby-3.1.2/ext/mri/siteconf20131218-21008-jcd3kl.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131218-21008-1ez23rk"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
